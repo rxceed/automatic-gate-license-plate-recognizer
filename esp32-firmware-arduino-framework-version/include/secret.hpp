c6505f9f3fd81eb340c04f382f1510dca7f600bf
Risang Radityatama <161427850+rxceed@users.noreply.github.com>
2025-12-10 10:03:26 +0700

    Update secret.hpp

--- esp32-firmware-arduino-framework-version/include/secret.hpp
+++ esp32-firmware-arduino-framework-version/include/secret.hpp
@@ -1,9 +1,9 @@
 #ifndef SECRET_HPP
 #define SECRET_HPP
 
-#define ENV_SSID "b401_wifi"
-#define ENV_WIFI_PASSWORD "b401juara1"
-#define ENV_MQTT_BROKER "192.168.200.118"
+#define ENV_SSID "SSID"
+#define ENV_WIFI_PASSWORD "PASS"
+#define ENV_MQTT_BROKER "IP"
 #define ENV_MQTT_PORT 1883
 
 #endif
